4d21b175f84d4892a08fd32e01fe0f6b8c643ab8
asafonov <me@asafonov.org>
2021-03-17 11:14:03 +0300

    updated

--- setup/node.sh
+++ setup/node.sh
@@ -2,10 +2,10 @@ export WORK=/home/asafonov/work
 sudo apt-get install bzip2 wget xz-utils
 cd $WORK
 rm -rf $WORK/node*
-wget https://nodejs.org/dist/v12.16.3/node-v12.16.3-linux-x64.tar.xz
-tar -xvf node-v12.16.3-linux-x64.tar.xz
-rm node-v12.16.3-linux-x64.tar.xz
+wget https://nodejs.org/dist/v14.16.0/node-v14.16.0-linux-x64.tar.xz
+tar -xvf node-v14.16.0-linux-x64.tar.xz
+rm node-v14.16.0-linux-x64.tar.xz
 sudo rm /usr/bin/node
 sudo rm /usr/bin/npm
-sudo ln -s $WORK/node-v12.16.3-linux-x64/bin/node /usr/bin/node
-sudo ln -s $WORK/node-v12.16.3-linux-x64/bin/npm /usr/bin/npm
+sudo ln -s $WORK/node-v14.16.0-linux-x64/bin/node /usr/bin/node
+sudo ln -s $WORK/node-v14.16.0-linux-x64/bin/npm /usr/bin/npm
--- setup/work.sh
+++ setup/work.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 su -c "apt-get install -y bash-completion tmux mc lynx python3-setuptools vim imagemagick php-cli davfs2 mutt"
-cp .gitconfig ~
-cp .tmux.conf ~
 cp .vimrc ~
 cp .muttrc ~
 cp .mailcap ~
